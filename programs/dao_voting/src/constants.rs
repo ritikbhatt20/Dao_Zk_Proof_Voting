cc158b03e0da91db2881d0befbc9c622ac11c2d1
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-07-12 22:58:38 +0530

    Feat: Added seeds and other stuff

---
+++ programs/dao_voting/src/constants.rs
@@ -0,0 +1,2 @@
+pub const ELECTION_SEED: &str = "election";
+pub const CHANGABLE_TOKEN_SEED: &str = "changabletoken";
--- programs/dao_voting/src/lib.rs
+++ programs/dao_voting/src/lib.rs
@@ -1,8 +1,15 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 use anchor_spl::token::{Token, TokenAccount};
+use solana_program::account_info::AccountInfo;
+use solana_program::program_pack::Pack;
+use spl_token::state::Account as SplTokenAccount;
 
-declare_id!("9bT97oZXoHPW41MayXtEeX4VoUaqkUE1TJvAQkSffRSH");
+mod constants;
+
+use crate::{constants::*};
+
+declare_id!("9ySQMPpE4RmyJ5ge2yyyMhzNrkLydX6JN56gzEcUuF1t");
 
 #[program]
 pub mod dao_voting {
@@ -10,17 +17,20 @@ pub mod dao_voting {
 
     pub fn new_polling(
         ctx: Context<NewPolling>,
-        proposal_voting: String,
+        token: Pubkey,
+        proposal_voting: String,                 
         value: String,
-        additional_value: String,
+        additional_value: String,           
     ) -> Result<()> {
         let election = &mut ctx.accounts.election;
+
         require!(!election.vote_active, CustomError::VoteActive);
 
-        let balance = ctx.accounts.token_account.balance_of(ctx.accounts.authority.key())?;
+        let balance = get_token_balance(&ctx.accounts.token_account.to_account_info())?;
         require!(balance > 0, CustomError::InsufficientBalance);
 
         election.id = Clock::get().unwrap().unix_timestamp as u64; // Unique identifier
+        election.token = token;
         election.proposal_voting = proposal_voting;
         election.value = value;
         election.additional_value = additional_value;
@@ -34,7 +44,7 @@ pub mod dao_voting {
         let election = &mut ctx.accounts.election;
         require!(election.vote_active, CustomError::VoteInactive);
 
-        let balance = ctx.accounts.token_account.balance_of(ctx.accounts.authority.key())?;
+        let balance = get_token_balance(&ctx.accounts.token_account.to_account_info())?;
         require!(balance > 0, CustomError::InsufficientBalance);
         require!(!election.voters.contains(&ctx.accounts.authority.key()), CustomError::AlreadyVoted);
 
@@ -53,12 +63,12 @@ pub mod dao_voting {
         require!(election.vote_active, CustomError::VoteInactive);
         require!(Clock::get().unwrap().unix_timestamp > election.time || election.number_of_votes >= election.min_votes, CustomError::VotingTime);
 
-        if election.current > 0 {
+        if election.current >= 0 {
             if election.proposal_voting == "newSymbol" {
-                ctx.accounts.token_account.change_symbol(election.value.clone())?;
+                ctx.accounts.changable_token_account.change_symbol(election.value.clone())?;
             }
             if election.proposal_voting == "newName" {
-                ctx.accounts.token_account.change_name(election.value.clone())?;
+                ctx.accounts.changable_token_account.change_name(election.value.clone())?;
             }
         }
 
@@ -82,6 +92,7 @@ pub mod dao_voting {
 #[account]
 pub struct Election {
     pub id: u64, // Unique identifier for the proposal
+    pub token: Pubkey,
     pub proposal_voting: String,
     pub value: String,
     pub additional_value: String,
@@ -97,12 +108,26 @@ pub struct Election {
 
 #[derive(Accounts)]
 pub struct NewPolling<'info> {
-    #[account(init, payer = authority, space = 8 + 64 + 256 + 256 + 8 + 8 + 8 + 1 + 32 + 8 + 1024)]
+    #[account(
+        init,
+        payer = authority, 
+        space = 8 + 64 + 256 + 256 + 8 + 8 + 8 + 1 + 32 + 8 + 1024,
+        seeds = [ELECTION_SEED.as_bytes(), authority.key().as_ref()],
+        bump
+    )]
     pub election: Account<'info, Election>,
     #[account(mut)]
     pub authority: Signer<'info>,
-    #[account(init, payer = authority, space = 8 + 32 + 256 + 256 + 8 + 8 + 32)]
-    pub token_account: Account<'info, ChangableTokenAccount>, // Initialize the token account
+    #[account(mut)]
+    pub token_account: Account<'info, TokenAccount>,
+    #[account(
+        init, 
+        payer = authority, 
+        space = 8 + 32 + 256 + 256 + 8 + 8 + 32,
+        seeds = [CHANGABLE_TOKEN_SEED.as_bytes(), authority.key().as_ref()],
+        bump
+    )]
+    pub changable_token_account: Account<'info, ChangableTokenAccount>, // Initialize the token account
     pub system_program: Program<'info, System>,
 }
 
@@ -113,7 +138,9 @@ pub struct Vote<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
     #[account(mut)]
-    pub token_account: Account<'info, ChangableTokenAccount>,
+    pub token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub changable_token_account: Account<'info, ChangableTokenAccount>,
 }
 
 #[derive(Accounts)]
@@ -123,7 +150,9 @@ pub struct ToSumUp<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
     #[account(mut)]
-    pub token_account: Account<'info, ChangableTokenAccount>,
+    pub token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub changable_token_account: Account<'info, ChangableTokenAccount>,
 }
 
 #[derive(Accounts)]
@@ -173,3 +202,10 @@ pub trait ChangableToken {
     fn change_name(&mut self, name: String) -> ProgramResult;
     fn balance_of(&self, user: Pubkey) -> Result<u64>;
 }
+
+fn get_token_balance(account: &AccountInfo) -> Result<u64> {
+    let data = &account.try_borrow_data()?;
+    let token_account = SplTokenAccount::unpack(data)?;
+    Ok(token_account.amount)
+}
+
