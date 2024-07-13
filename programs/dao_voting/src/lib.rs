c03d777524160fba854315c4a4726f58c78f2141
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-07-14 00:31:10 +0530

    Big Feat: Integrated verification of zk proofs

--- programs/dao_voting/src/lib.rs
+++ programs/dao_voting/src/lib.rs
@@ -1,26 +1,33 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
-use anchor_spl::token::{Token, TokenAccount};
+use anchor_spl::token::{self, TokenAccount};
 use solana_program::account_info::AccountInfo;
 use solana_program::program_pack::Pack;
 use spl_token::state::Account as SplTokenAccount;
+use bellman::groth16::{Proof, prepare_verifying_key, verify_proof};
+use bls12_381::{Bls12, Scalar};
+use std::str::FromStr;
+use bellman::groth16::VerifyingKey as GrothVerifyingKey;
+use anchor_lang::error::AnchorError;
 
 mod constants;
 
 use crate::{constants::*};
 
-declare_id!("9ySQMPpE4RmyJ5ge2yyyMhzNrkLydX6JN56gzEcUuF1t");
+declare_id!("3XuNmJEHjuk5Vo7U6fAPp1vJekyW2GJsSmWBWkLjnbyK");
 
 #[program]
 pub mod dao_voting {
+    use std::num::NonZeroI128;
+
     use super::*;
 
     pub fn new_polling(
         ctx: Context<NewPolling>,
         token: Pubkey,
-        proposal_voting: String,                 
+        proposal_voting: String,
         value: String,
-        additional_value: String,           
+        additional_value: String,
     ) -> Result<()> {
         let election = &mut ctx.accounts.election;
 
@@ -40,14 +47,40 @@ pub mod dao_voting {
         Ok(())
     }
 
-    pub fn vote(ctx: Context<Vote>, vote: bool) -> Result<()> {
+    pub fn vote(
+        ctx: Context<Vote>, 
+        vote: bool, 
+        zk_proof: Vec<u8>, 
+        public_input: String
+    ) -> Result<()> {
         let election = &mut ctx.accounts.election;
         require!(election.vote_active, CustomError::VoteInactive);
-
+    
         let balance = get_token_balance(&ctx.accounts.token_account.to_account_info())?;
         require!(balance > 0, CustomError::InsufficientBalance);
         require!(!election.voters.contains(&ctx.accounts.authority.key()), CustomError::AlreadyVoted);
-
+    
+        // Deserialize and verify zk-SNARK proof
+        let proof = Proof::<Bls12>::read(&zk_proof[..]).map_err(|_| CustomError::ProofDeserializationFailed)?;
+    
+        // Convert public input from string to Scalar
+        let bytes = hex::decode(&public_input).map_err(|_| CustomError::InvalidPublicInput)?;
+        require!(bytes.len() == 32, CustomError::InvalidPublicInput); // Ensure bytes length is correct
+        
+        let mut array = [0u8; 64]; // Use a 64-byte array
+        array[..32].copy_from_slice(&bytes); // Copy the decoded bytes into the first half
+    
+        // Convert from bytes to Scalar
+        let scalar = Scalar::from_bytes_wide(&array);
+        
+        let verifying_key_bytes = ctx.accounts.verifying_key.key.as_slice();
+        let vk = GrothVerifyingKey::<Bls12>::read(verifying_key_bytes).map_err(|_| CustomError::ProofVerificationFailed)?;
+        let pvk = prepare_verifying_key(&vk);
+        let result = verify_proof(&pvk, &proof, &[scalar]);
+        if result.is_err() {
+            return Err(CustomError::ProofVerificationFailed.into());
+        }
+    
         if vote {
             election.current += balance as i64;
         } else {
@@ -57,19 +90,35 @@ pub mod dao_voting {
         election.voters.push(ctx.accounts.authority.key()); // Add voter to the list
         Ok(())
     }
+    
 
     pub fn to_sum_up(ctx: Context<ToSumUp>) -> Result<()> {
         let election = &mut ctx.accounts.election;
         require!(election.vote_active, CustomError::VoteInactive);
         require!(Clock::get().unwrap().unix_timestamp > election.time || election.number_of_votes >= election.min_votes, CustomError::VotingTime);
 
-        if election.current >= 0 {
+        if election.current > 0 {
             if election.proposal_voting == "newSymbol" {
                 ctx.accounts.changable_token_account.change_symbol(election.value.clone())?;
             }
             if election.proposal_voting == "newName" {
                 ctx.accounts.changable_token_account.change_name(election.value.clone())?;
             }
+        }
+
+        Ok(())
+    }
+
+    pub fn get_results(ctx: Context<GetResults>) -> Result<()> {
+        let election = &mut ctx.accounts.election;
+
+        msg!("Total number of votes : {}", election.number_of_votes);
+        msg!("Current Votes Balance : {}", election.current);
+
+        if election.current > 0 {
+            msg!("Proposal Passed");
+        } else {
+            msg!("Proposal Rejected");
         }
 
         election.vote_active = false;
@@ -79,13 +128,14 @@ pub mod dao_voting {
         election.value = String::new();
         election.additional_value = String::new();
         election.voters.clear(); // Clear the list of voters
-
         Ok(())
     }
 
-    pub fn get_results(ctx: Context<GetResults>) -> Result<(i64, u64)> {
-        let election = &ctx.accounts.election;
-        Ok((election.current, election.number_of_votes))
+    pub fn close_election(ctx: Context<CloseElection>) -> Result<()> {
+        let election = &mut ctx.accounts.election;
+        require!(!election.vote_active, CustomError::VoteActive);
+
+        Ok(())
     }
 }
 
@@ -128,50 +178,80 @@ pub struct NewPolling<'info> {
         bump
     )]
     pub changable_token_account: Account<'info, ChangableTokenAccount>, // Initialize the token account
+    pub verifying_key: Account<'info, VerifyingKey>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 pub struct Vote<'info> {
-    #[account(mut)]
+    #[account(
+        mut,
+        seeds = [ELECTION_SEED.as_bytes(), election.creator.as_ref()],
+        bump,
+    )]
     pub election: Account<'info, Election>,
     #[account(mut)]
     pub authority: Signer<'info>,
     #[account(mut)]
     pub token_account: Account<'info, TokenAccount>,
-    #[account(mut)]
+    #[account(
+        mut,
+        seeds = [CHANGABLE_TOKEN_SEED.as_bytes(), election.creator.as_ref()],
+        bump,
+    )]
     pub changable_token_account: Account<'info, ChangableTokenAccount>,
+    pub verifying_key: Account<'info, VerifyingKey>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 pub struct ToSumUp<'info> {
-    #[account(mut)]
+    #[account(
+        mut,
+        seeds = [ELECTION_SEED.as_bytes(), election.creator.as_ref()],
+        bump,
+    )]
     pub election: Account<'info, Election>,
     #[account(mut)]
     pub authority: Signer<'info>,
     #[account(mut)]
     pub token_account: Account<'info, TokenAccount>,
-    #[account(mut)]
+    #[account(
+        mut,
+        seeds = [CHANGABLE_TOKEN_SEED.as_bytes(), election.creator.as_ref()],
+        bump,
+    )]
     pub changable_token_account: Account<'info, ChangableTokenAccount>,
 }
 
 #[derive(Accounts)]
 pub struct GetResults<'info> {
+    #[account(
+        mut,
+        seeds = [ELECTION_SEED.as_bytes(), election.creator.as_ref()],
+        bump,
+    )]
     pub election: Account<'info, Election>,
 }
 
-#[error_code]
-pub enum CustomError {
-    #[msg("Vote is already active.")]
-    VoteActive,
-    #[msg("Vote is not active.")]
-    VoteInactive,
-    #[msg("Insufficient balance.")]
-    InsufficientBalance,
-    #[msg("Voting time has not ended or minimum votes not reached.")]
-    VotingTime,
-    #[msg("User has already voted.")]
-    AlreadyVoted,
+#[derive(Accounts)]
+pub struct CloseElection<'info> {
+    #[account(
+        mut,
+        close = authority,
+        seeds = [ELECTION_SEED.as_bytes(), election.creator.as_ref()],
+        bump
+    )]
+    pub election: Account<'info, Election>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(
+        mut,
+        close = authority,
+        seeds = [CHANGABLE_TOKEN_SEED.as_bytes(), election.creator.as_ref()],
+        bump
+    )]
+    pub changable_token_account: Account<'info, ChangableTokenAccount>,
 }
 
 #[account]
@@ -203,9 +283,33 @@ pub trait ChangableToken {
     fn balance_of(&self, user: Pubkey) -> Result<u64>;
 }
 
+#[account]
+pub struct VerifyingKey {
+    pub key: Vec<u8>,
+}
+
+#[error_code]
+pub enum CustomError {
+    #[msg("Vote is already active.")]
+    VoteActive,
+    #[msg("Vote is not active.")]
+    VoteInactive,
+    #[msg("Insufficient balance.")]
+    InsufficientBalance,
+    #[msg("Voting time has not ended or minimum votes not reached.")]
+    VotingTime,
+    #[msg("User has already voted.")]
+    AlreadyVoted,
+    #[msg("Proof deserialization failed.")]
+    ProofDeserializationFailed,
+    #[msg("Invalid public input.")]
+    InvalidPublicInput,
+    #[msg("Proof verification failed.")]
+    ProofVerificationFailed,
+}
+
 fn get_token_balance(account: &AccountInfo) -> Result<u64> {
     let data = &account.try_borrow_data()?;
     let token_account = SplTokenAccount::unpack(data)?;
     Ok(token_account.amount)
 }
-
